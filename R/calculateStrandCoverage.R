3c040467325fa415ca0068552977f7133a1bfb5f
tothuhien <tothuhien@gmail.com>
2017-09-11 07:59:24 +0000

    check
    Former-commit-id: fda4466216a605684d428cdd129953652c365ffb

--- R/calculateStrandCoverage.R
+++ R/calculateStrandCoverage.R
@@ -3,7 +3,7 @@
 #' @description Calculate the coverage coming from '+'/'-' reads in all sliding wndows
 #' 
 #' @param winPositiveAlignments a list that has a `Coverage` field that contains coverage coming from positive reads
-#' @param winNegativeAlignmentsa a list that has a `Coverage` field that contains coverage coming from negative reads
+#' @param winNegativeAlignments a list that has a `Coverage` field that contains coverage coming from negative reads
 #' @param winWidth the length of the sliding window, 1000 by default.
 #' @param winStep the step length to sliding the window, 100 by default.
 #' @importFrom IRanges Views
